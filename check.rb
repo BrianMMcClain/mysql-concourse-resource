d0b26805fff55bab32ececaeccf4ca02ad28eb10
Brian McClain <brianmmcclain@gmail.com>
2018-04-03 16:43:20 -0400

    Add comments clarifying what the check script is doing

--- check.rb
+++ check.rb
@@ -14,6 +14,7 @@
 require 'mysql2'
 require 'json'
 
+# Parse the parameters passed to the script over STDIN
 sSource = ARGF.read
 source = JSON.parse(sSource)
 
@@ -22,16 +23,16 @@ user = source["source"]["user"]
 password = source["source"]["password"]
 database = source["source"]["database"]
 
-STDERR.puts source
-STDERR.puts source["version"]
-
+# For the first build, the version field will be null, so let's handle that edge case
 previous_version = 0
 if source.has_key?("version") and not source["version"].nil?
     previous_version = source["version"]["id"]
 end
 
+# Pull down all new rows, determined by the 'id' primary key
 client = Mysql2::Client.new(:host => host, :username => user, :password => password, :database => database)
 
+# Build the result and send to STDOUT
 ret = []
 results = client.query("SELECT * FROM concourseresource WHERE id > #{previous_version};")
 results.each do |r|
